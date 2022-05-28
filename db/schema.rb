7f1ec9237e3aa4e2c5bd5205e65ee653af09001c
ZeldaMazzy <hello@ezekielhickson.com>
2022-05-27 20:23:37 -0500

    Added "status" to projects table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_24_120905) do
+ActiveRecord::Schema.define(version: 2022_05_28_012202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2022_05_24_120905) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "vendors_id"
     t.integer "talent_needed"
+    t.string "status"
     t.index ["user_id"], name: "index_projects_on_user_id"
     t.index ["vendors_id"], name: "index_projects_on_vendors_id"
   end
